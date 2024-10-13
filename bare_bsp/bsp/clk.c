fe84585adaeeb009d06cd5c4cfcf4e104e65c3e3
xcy <xcy@qq.com>
2024-10-13 15:11:48 +0800

    printf and uart

--- bare_bsp/bsp/clk.c
+++ bare_bsp/bsp/clk.c
@@ -65,19 +65,9 @@ void clk_init(void)
         CCM->CBCDR &= ~(1 << 25);	/* periph_clk=pre_periph_clk=396MHz */
         while(CCM->CDHIPR & (1 << 5));/* 等待握手完成 */
             
-        /* 修改AHB_PODF位的时候需要先禁止AHB_CLK_ROOT的输出，但是
-        * 我没有找到关闭AHB_CLK_ROOT输出的的寄存器，所以就没法设置。
-        * 下面设置AHB_PODF的代码仅供学习参考不能直接拿来使用！！
-        * 内部boot rom将AHB_PODF设置为了3分频，即使我们不设置AHB_PODF，
-        * AHB_ROOT_CLK也依旧等于396/3=132Mhz。
-        */
-    #if 0
-        /* 要先关闭AHB_ROOT_CLK输出，否则时钟设置会出错 */
         CCM->CBCDR &= ~(7 << 10);	/* CBCDR的AHB_PODF清零 */
         CCM->CBCDR |= 2 << 10;		/* AHB_PODF 3分频，AHB_CLK_ROOT=132MHz */
-        while(CCM->CDHIPR & (1 << 1));/
-    * 等待握手完成 */
-    #endif
+        while(CCM->CDHIPR & (1 << 1));
         
         /* 5、设置IPG_CLK_ROOT最小3Mhz，最大66Mhz (boot rom自动设置好了可以不用设置)*/
         CCM->CBCDR &= ~(3 << 8);	/* CBCDR的IPG_PODF清零 */
@@ -86,4 +76,8 @@ void clk_init(void)
         /* 6、设置PERCLK_CLK_ROOT时钟 */
         CCM->CSCMR1 &= ~(1 << 6);	/* PERCLK_CLK_ROOT时钟源为IPG */
         CCM->CSCMR1 &= ~(7 << 0);	/* PERCLK_PODF位清零，即1分频 */
+        
+        /* 7、设置UART时钟 */
+        CCM->CSCDR1 &= ~(1 << 6);
+        CCM->CSCDR1 &= ~(0x3f << 0);
 }
--- bare_bsp/project/main.c
+++ bare_bsp/project/main.c
@@ -6,21 +6,24 @@
 #include "gpt.h"
 #include "delay.h"
 #include "beep.h"
+#include "uart.h"
 #include "imx6ull.h"
+#include "stdio.h"
 
 int main(void)
 {   
+	int a , b;
     int_init();
     clk_init();
     clk_enable();
-    delay_init();	
+    uart_init();
     led_init();
+    unsigned char c = 0;
     while(1)
     {   
-        led_on();
-        delay_ms(10000);
-        led_off();
-        delay_ms(10000);
+        printf("输入两个整数，使用空格隔开:");
+		scanf("%d %d", &a, &b);					 		/* 输入两个整数 */
+		printf("\r\n数据%d + %d = %d\r\n\r\n", a, b, a+b);	/* 输出两个数相加的和 */
     }
     return 0;
 }
